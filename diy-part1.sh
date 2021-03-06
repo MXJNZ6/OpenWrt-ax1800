ca68cbec5f6c3bac8717ca2e003de00abcb4e2a4
MXJNZ6 <53927877+MXJNZ6@users.noreply.github.com>
2021-03-06 12:29:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,4 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/jerrykuku/luci-app-vssr.git  package/luci-app-vssr
