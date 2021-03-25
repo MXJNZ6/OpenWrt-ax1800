4a09e1d21f367e7ae462a180dea39850c8dcb538
MXJNZ6 <53927877+MXJNZ6@users.noreply.github.com>
2021-03-25 11:02:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -29,5 +29,5 @@ svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-fileassistant pa
 git clone https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
 git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
-git clone https://github.com/firker/diy-ziyong package/diy-ziyong
+git clone https://github.com/MXJNZ6/diy-ziyong package/diy-ziyong
 git clone https://github.com/sirpdboy/luci-theme-opentopd package/luci-theme-opentopd
