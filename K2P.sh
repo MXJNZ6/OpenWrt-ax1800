6fe0446e5c6344da431801e76dfe12988a442c69
MXJ <53927877+MXJNZ6@users.noreply.github.com>
2021-07-07 14:47:13 +0800

    Update K2P.sh

--- K2P.sh
+++ K2P.sh
@@ -11,7 +11,7 @@
 #
 
 # 修改openwrt登陆地址
-sed -i 's/192.168.2.3/192.168.2.2/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
 
 # Installation dependencies
 #find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {} && find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
