92badd807b11486cc15a167a6618375ccd6b3e9f
MXJ <53927877+MXJNZ6@users.noreply.github.com>
2022-02-28 19:06:43 +0800

    Update X86.sh

--- X86.sh
+++ X86.sh
@@ -10,14 +10,8 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# 修改openwrt登陆地址
-sed -i 's/192.168.1.1/10.10.10.254/g' package/base-files/files/bin/config_generate
-
 #Cancel the default theme
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-
-# 修改主机名字，把OpenWrt-CE修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i '/uci commit system/i\uci set system.@system[0].hostname='iKuai-unicorn'' package/lean/default-settings/files/zzz-default-settings
 
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 #sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
