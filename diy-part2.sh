f704be5646f46797780e000fcddfeea635504d70
MXJ <53927877+MXJNZ6@users.noreply.github.com>
2022-04-15 09:16:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,11 +1,27 @@
-#删除重复包
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/packages/net/aliyundrive-webdav
-rm -rf feeds/luci/applications/luci-app-ocserv
-mv package/luci-app-ocserv feeds/luci/applications
-rm -rf feeds/luci/applications/luci-app-dockerman
-rm -rf feeds/packages/multimedia/UnblockNeteaseMusic
-rm -rf feeds/luci/applications/luci-app-unblockmusic
-#rm -rf feeds/luci/applications/luci-app-openvpn-server
-rm -rf feeds/packages/multimedia/UnblockNeteaseMusic-Go
-rm -rf feeds/luci/applications/luci-app-aliyundrive-webdav
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# luci-app-argon-config
+sed -i 's/system/services/g'  package/luci-app-argon-config/luasrc/controller/argon-config.lua
+
+# 调整VPN服务到VPN菜单
+sed -i 's/status/vpn/g' feeds/luci/applications/luci-app-wireguard/luasrc/controller/wireguard.lua
+sed -i 's/92/2/g' feeds/luci/applications/luci-app-wireguard/luasrc/controller/wireguard.lua
+
+# 调整阿里云盘到存储菜单
+sed -i 's/services/nas/g' package/luci-app-aliyundrive-webdav/luci-app-aliyundrive-webdav/luasrc/controller/*.lua
+sed -i 's/services/nas/g' package/luci-app-aliyundrive-webdav/luci-app-aliyundrive-webdav/luasrc/model/cbi/aliyundrive-webdav/*.lua
+sed -i 's/services/nas/g' package/luci-app-aliyundrive-webdav/luci-app-aliyundrive-webdav/luasrc/view/aliyundrive-webdav/*.htm
+
+# 修改插件名字
+sed -i 's/"阿里云盘 WebDAV"/"阿里云盘"/g' package/luci-app-aliyundrive-webdav/luci-app-aliyundrive-webdav/po/zh-cn/aliyundrive-webdav.po
+sed -i 's/WireGuard 状态/WireGuard/g' feeds/luci/applications/luci-app-wireguard/po/zh-cn/wireguard.po
