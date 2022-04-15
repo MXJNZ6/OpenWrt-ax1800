56a5a21643ab04b18a92430e4e0f87706347f172
MXJ <53927877+MXJNZ6@users.noreply.github.com>
2022-04-15 09:13:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,9 +13,6 @@
 # 修改openwrt登陆地址,把下面的192.168.2.22修改成你想要的就可以了
 sed -i 's/192.168.1.1/192.168.2.22/g' package/base-files/files/bin/config_generate
 
-# 修改主机名字，把Unicorn修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i '/uci commit system/i\uci set system.@system[0].hostname='Unicorn'' package/lean/default-settings/files/zzz-default-settings
-
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt package/luci-app-aliyundrive-webdav
 svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser package/luci-app-filebrowser
